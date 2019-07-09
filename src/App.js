768a471f66cba7c2f62ffce95ed82814124e8c6b
Anaclet Biquet <anaclet.biquet@gmail.com>
2019-07-09 17:58:07 +0200

    state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Lamp from './Lamp';
+import Quotes from "./Quotes";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      working : true,
+      class : "App-logo"
+    }
+  }
+  changeHomerstate = () => {
+    this.setState({working : !this.state.working, class : this.state.class === "App-logo" ? "App-logo2" :"App-logo" });    
+  };
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        <img src={logo} className={this.state.class} alt="logo" />
+        <h1 className="App-title">Simpsons Quotes</h1>
+        </header>        
+        <Lamp/>        
+        <Quotes/>
+        <button onClick={this.changeHomerstate}>Homer</button>
+      </div>      
+    );
+  }
 }
 
 export default App;
